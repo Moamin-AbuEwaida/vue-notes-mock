871a8b3e6ff11a3016d7c876f369ced06cc2fb85
Moamin <moamin.salamah@gmail.com>
2025-03-11 16:25:34 +0100

    1st push

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Sidebar from "./components/Sidebar.vue";
+</script>
+
+<template>
+  <div
+    class="bg-gray-700 text-white min-h-screen flex items-stretch justify-start"
+  >
+    <!-- sidebar -->
+    <Sidebar />
+    <!-- main content -->
+    <main class="flex-1">
+      <div class="px-4 py-8 flex flex-col h-full">
+        <input
+          type="text"
+          class="block w-full text-3xl pb-2 font-bold border-b-2 border-gray-500 focus:border-white outline-none transition-colors duration-200"
+        />
+      </div>
+    </main>
+  </div>
+</template>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,23 @@
+<template>
+  <aside class="w-[256px] min-h-screen bg-gray-800">
+    <div class="p-4">
+      <button class="button w-full">New Note</button>
+    </div>
+    <ul>
+      <li
+        class="p-4 border-b border-gray-700 hover:bg-gray-600 cursor-pointer flex justify-between items-center gap-4"
+      >
+        <span>Note Title</span>
+        <i
+          class="pi pi-times text-red-600 transition-transform duration-50 hover:rotate-30"
+        ></i>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
