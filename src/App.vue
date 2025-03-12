85af1e23c9eec5891614dc9526b0c0d67eba287e
Moamin <moamin.salamah@gmail.com>
2025-03-12 11:01:16 +0100

    working

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,51 @@
 <script setup>
+import { ref } from "vue";
 import Sidebar from "./components/Sidebar.vue";
+
+const notes = ref([]);
+const activeNote = ref(null);
+const inputTitle = ref("");
+const noteContent = ref("");
+
+// create note
+const createNote = () => {
+  // random id
+  const id = Math.random().toString(36).substring(2, 9);
+  notes.value.push({
+    id,
+    title: "untitled",
+    content: "",
+  });
+  setActiveNote(id);
+};
+// set Active note
+const setActiveNote = (id) => {
+  activeNote.value = id;
+  let note = notes.value.find((note) => note.id === id);
+
+  inputTitle.value = note.title;
+  noteContent.value = note.content;
+
+  setTimeout(() => {
+    document.querySelector('input[type="text"]').focus();
+  }, 0);
+};
+// delete note
+const deleteNote = ({ id, e }) => {
+  e.stopPropagation();
+  let note = notes.value.findIndex((note) => note.id === id);
+  notes.value.splice(note, 1);
+
+  activeNote.value = null;
+  inputTitle.value = "";
+  noteContent.value = "";
+};
+// update note
+const updateNote = () => {
+  let note = notes.value.findIndex((note) => note.id === activeNote.value);
+  notes.value[note].title = inputTitle.value;
+  notes.value[note].content = noteContent.value;
+};
 </script>
 
 <template>
@@ -7,14 +53,27 @@ import Sidebar from "./components/Sidebar.vue";
     class="bg-gray-700 text-white min-h-screen flex items-stretch justify-start"
   >
     <!-- sidebar -->
-    <Sidebar />
+    <Sidebar
+      :activeNote="activeNote"
+      :notes="notes"
+      @newNote="createNote"
+      @setActiveNote="setActiveNote"
+      @deleteNote="deleteNote"
+    />
     <!-- main content -->
     <main class="flex-1">
-      <div class="px-4 py-8 flex flex-col h-full">
+      <div v-if="activeNote" class="px-4 py-8 flex flex-col h-full">
         <input
+          v-model="inputTitle"
+          @input="updateNote"
           type="text"
           class="block w-full text-3xl pb-2 font-bold border-b-2 border-gray-500 focus:border-white outline-none transition-colors duration-200"
         />
+        <textarea
+          v-model="noteContent"
+          @input="updateNote"
+          class="block w-full h-full mt-4 text-lg outline-none flex-1"
+        ></textarea>
       </div>
     </main>
   </div>
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,23 +1,25 @@
+<script setup>
+const props = defineProps(["activeNote", "notes"]);
+</script>
+
 <template>
   <aside class="w-[256px] min-h-screen bg-gray-800">
     <div class="p-4">
-      <button class="button w-full">New Note</button>
+      <button class="button w-full" @click="$emit('newNote')">New Note</button>
     </div>
     <ul>
       <li
+        v-for="note in props.notes"
+        :key="note.id"
+        @click="$emit('setActiveNote', note.id)"
         class="p-4 border-b border-gray-700 hover:bg-gray-600 cursor-pointer flex justify-between items-center gap-4"
       >
-        <span>Note Title</span>
+        <span>{{ note.title }}</span>
         <i
+          @click="(e) => $emit('deleteNote', { id: note.id, e })"
           class="pi pi-times text-red-600 transition-transform duration-50 hover:rotate-30"
         ></i>
       </li>
     </ul>
   </aside>
 </template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
